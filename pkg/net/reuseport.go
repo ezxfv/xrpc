60a0a9055f3eba2be9395f12c5327b642a58a33b
edenzhong <edenzhong@tencent.com>
2020-02-22 13:20:10 +0800

    add net alias

--- pkg/net/reuseport.go
+++ pkg/net/reuseport.go
@@ -1,10 +1,19 @@
 package net
 
 import (
+	"net"
+
 	reuseport "github.com/libp2p/go-reuseport"
 )
 
+type (
+	PacketConn = net.PacketConn
+)
+
 var (
-	TCPListen = reuseport.Listen
-	UDPListen = reuseport.ListenPacket
+	TCPListen       = reuseport.Listen
+	UDPListen       = reuseport.ListenPacket
+	ResolveTCPAddr  = net.ResolveTCPAddr
+	ResolveUDPAddr  = net.ResolveUDPAddr
+	ResolveUnixAddr = net.ResolveUnixAddr
 )
